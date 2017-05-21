e04bc9419aed2330d1fdd684fdb07f78f039e72a
Joanna Gaudyn <aska.gaudyn@gmail.com>
2017-05-21 21:28:46 +0100

    heroku migrations fail, trying to figure out why

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 20170519092757) do
     t.string "photo"
   end
 
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "doses", force: :cascade do |t|
     t.string "description"
     t.bigint "cocktail_id"
@@ -32,11 +38,6 @@ ActiveRecord::Schema.define(version: 20170519092757) do
     t.index ["ingredient_id"], name: "index_doses_on_ingredient_id"
   end
 
-  create_table "ingredients", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   add_foreign_key "doses", "cocktails"
   add_foreign_key "doses", "ingredients"
